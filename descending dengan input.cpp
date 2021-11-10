99a0bb7b7b1ab7911f4263dff7adc7917806bdb9
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-10 17:01:30 +0700

    CODE

---
+++ descending dengan input.cpp
@@ -0,0 +1,40 @@
+// descending = besar ke kecil
+
+#include <iostream>
+using namespace std;
+int main(){
+	
+	bool swapped;
+	int temp;     //untuk menyimpan nilai sementara
+	int IndexOfLastSortedElement = 0;
+	
+	cout << "Masukkan Jumlah Elemen : ";
+	cin  >> IndexOfLastSortedElement;
+	
+	int array[IndexOfLastSortedElement];
+	
+	for(int i=0; i<IndexOfLastSortedElement; i++){
+		cout << "Masukkan Elemen Ke " << i << " : ";
+		cin  >> array[i];
+	}
+	
+	do{
+		swapped = false;
+		
+		for(int i=0; i<IndexOfLastSortedElement-1; i++){ // -1 karena index array tidak bisa diisi semua
+			
+			if(array[i]<array[i+1]){  //apabila bilangan ke-i lebih kecil daripada bilangan setelahnya
+				
+				temp       = array[i];
+				array[i]   = array[i+1];
+				array[i+1] = temp;
+				
+				swapped = true;
+			}
+		}
+	} while(swapped);
+	
+	for(int i=0; i<IndexOfLastSortedElement; i++){
+		cout << array[i] << " ";
+	}
+}
